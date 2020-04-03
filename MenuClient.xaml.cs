54b5a4136d393d30a9bc9513a6f0db409a5a2856
PavelonBas <63090150+PavelonBas@users.noreply.github.com>
2020-04-03 20:22:10 +0300

    Повесил событие нажатие на кнопку при котором комната становится зарезервированной

--- MenuClient.xaml.cs
+++ MenuClient.xaml.cs
@@ -36,7 +36,26 @@ namespace BasovHotel
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            AppData.Context.Rooms.Add(new Rooms()
+            {
+                id_Room = Convert.ToInt32(CbHr.SelectedItem),
+                Num_of_beds_room = AppData.Context.Rooms.Where(p => Convert.ToInt32(CbHr.SelectedItem) == p.id_Room).Single().Num_of_beds_room,
+                Busy_room = true,
+                Price_room = AppData.Context.Rooms.Where(p => Convert.ToInt32(CbHr.SelectedItem) == p.id_Room).Single().Price_room,
+                Category = AppData.Context.Rooms.Where(p => Convert.ToInt32(CbHr.SelectedItem) == p.id_Room).Single().Category
+            });
+            AppData.Context.SaveChanges();
+            var rem = AppData.Context.Rooms.Where(p => (int)CbHr.SelectedItem == p.id_Room).FirstOrDefault();
+            AppData.Context.Rooms.Remove(rem as Rooms);
+            AppData.Context.SaveChanges();
+            AppData.Context.Distribution_rooms.Add(new Distribution_rooms()
+            {
+                Client = AppData.Context.Clients.Where(p => p.User_id == AppData.Context.Users.Where(a => a.Login == AppData.UserName).FirstOrDefault().id_User).FirstOrDefault().id_Client,
+                Room = (int)CbHr.SelectedItem
+            });
+            AppData.Context.SaveChanges();
+            MessageBox.Show($"{AppData.UserName} Спасибо за бронирование комнаты", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            AppData.MainFrame.GoBack();
         }
 
         private void CbService_SelectionChanged(object sender, SelectionChangedEventArgs e)
